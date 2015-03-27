41c1bb7b062c86bfeaedc51e0747f5bdbc2f8fee
Patrick Lambert <dendory@live.ca>
2015-03-27 18:30:20 -0400

    Initial code

---
+++ SupportRequest/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SupportRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlebox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.descriptionbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.urgentbox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // titlebox
+            // 
+            this.titlebox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titlebox.Location = new System.Drawing.Point(84, 13);
+            this.titlebox.MaxLength = 200;
+            this.titlebox.Name = "titlebox";
+            this.titlebox.Size = new System.Drawing.Size(628, 20);
+            this.titlebox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ticket title:";
+            // 
+            // descriptionbox
+            // 
+            this.descriptionbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descriptionbox.Location = new System.Drawing.Point(9, 61);
+            this.descriptionbox.Multiline = true;
+            this.descriptionbox.Name = "descriptionbox";
+            this.descriptionbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.descriptionbox.Size = new System.Drawing.Size(703, 195);
+            this.descriptionbox.TabIndex = 2;
+            this.descriptionbox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(479, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter your support request here. Please be descriptive and mention any error mess" +
+    "age you received:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(637, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // urgentbox
+            // 
+            this.urgentbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.urgentbox.AutoSize = true;
+            this.urgentbox.Location = new System.Drawing.Point(13, 274);
+            this.urgentbox.Name = "urgentbox";
+            this.urgentbox.Size = new System.Drawing.Size(145, 17);
+            this.urgentbox.TabIndex = 5;
+            this.urgentbox.Text = "This is an urgent request.";
+            this.urgentbox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 309);
+            this.Controls.Add(this.urgentbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.descriptionbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titlebox);
+            this.MinimumSize = new System.Drawing.Size(740, 347);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Support Request";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titlebox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox descriptionbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox urgentbox;
+    }
+}
+
---
+++ SupportRequest/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupportRequest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(titlebox.Text == "" || descriptionbox.Text == "")
+            {
+                MessageBox.Show("Please enter a title and description.", "Support Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
