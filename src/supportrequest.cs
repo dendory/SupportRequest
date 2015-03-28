6834f7ed03d6b0421268a4da0e50aee9c3c43180
Patrick Lambert <dendory@live.ca>
2015-03-28 12:49:13 -0400

    work on code

--- src/supportrequest.cs
+++ src/supportrequest.cs
@@ -20,14 +20,24 @@ namespace SupportRequest
 {
 	public class Program
 	{
-		public static void submit(string title, string description, string urgent)
+		static TextBox titlebox;
+		static Label label1;
+		static TextBox descriptionbox;
+		static Label label2;
+		static Button button1;
+		static CheckBox urgentbox;
+		static Form win;
+
+		// Submit new ticket
+		public static void submit()
 		{
-			if(title == "" || description == "")
+			if(titlebox.Text == "" || descriptionbox.Text == "")
 			{
 				MessageBox.Show("Please enter a title and description.", "Support Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 			else
 			{
+				button1.Enabled = false;
 				try
 				{
 					RegistryKey rkey = Registry.LocalMachine.OpenSubKey("Software\\SupportRequest");
@@ -41,9 +51,9 @@ namespace SupportRequest
 					wc.QueryString.Add("key", (string)rkey.GetValue("key"));
 					wc.QueryString.Add("product_id", (string)rkey.GetValue("product_id"));
 					wc.QueryString.Add("release_id", (string)rkey.GetValue("release_id"));
-					wc.QueryString.Add("title", title);
-					wc.QueryString.Add("description", "Support Request for: " + Environment.MachineName + "\\" + Environment.UserName + "\n\n" + description);
-					wc.QueryString.Add("custom", urgent);
+					wc.QueryString.Add("title", titlebox.Text);
+					wc.QueryString.Add("description", "Support Request for: " + Environment.MachineName + "\\" + Environment.UserName + "\n\n" + descriptionbox.Text);
+					wc.QueryString.Add("custom", urgentbox.Checked.ToString());
 					string result = wc.DownloadString((string)rkey.GetValue("url"));
 					if (result.Contains("OK")) { MessageBox.Show("Ticket added.", "Support Request"); }
 					else { MessageBox.Show("Ticket creation failed:\n\n" + result, "Support Request", MessageBoxButtons.OK, MessageBoxIcon.Error); }
@@ -52,21 +62,17 @@ namespace SupportRequest
 				{
 					MessageBox.Show("Ticket creation failed:\n\n" + ex, "Support Request", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				}
+				button1.Enabled = true;
 			}
 		}
 
+		// Make UI
 		public static void Main()
 		{
-			TextBox titlebox = new TextBox();
-			Label label1 = new Label();
-			TextBox descriptionbox = new TextBox();
-			Label label2 = new Label();
-			Button button1 = new Button();
-			CheckBox urgentbox = new CheckBox();
-			Form win = new Form();
-			win.SuspendLayout();
+			win = new Form();
 
 			// Title entry
+			titlebox = new TextBox();
 			titlebox.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
 			titlebox.Location = new System.Drawing.Point(84, 13);
 			titlebox.MaxLength = 200;
@@ -75,6 +81,7 @@ namespace SupportRequest
 			titlebox.TabIndex = 0;
 
 			// Title label
+			label1 = new Label();
 			label1.AutoSize = true;
 			label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			label1.Location = new System.Drawing.Point(6, 16);
@@ -84,6 +91,7 @@ namespace SupportRequest
 			label1.Text = "Ticket title:";
 
 			// Description box
+			descriptionbox = new TextBox();
 			descriptionbox.Anchor = ((AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right)));
 			descriptionbox.Location = new System.Drawing.Point(9, 61);
 			descriptionbox.Multiline = true;
@@ -93,17 +101,19 @@ namespace SupportRequest
 			descriptionbox.TabIndex = 2;
 
 			// Description label
-			label2.Anchor = ((AnchorStyles)(((AnchorStyles.Top | AnchorStyles.Left) | AnchorStyles.Right)));
+			label2 = new Label();
+			label2.Anchor = ((AnchorStyles)(AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right));
 			label2.AutoSize = true;
 			label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			label2.Location = new System.Drawing.Point(118, 45);
+			label2.Location = new System.Drawing.Point(118, 40);
 			label2.Name = "label2";
-			label2.Size = new System.Drawing.Size(479, 13);
+			label2.Size = new System.Drawing.Size(703, 13);
 			label2.TabIndex = 3;
 			label2.Text = "Enter your support request here. Please be descriptive and mention any error message you received:";
 			label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 
 			// Submit button
+			button1 = new Button();
 			button1.Anchor = ((AnchorStyles)((AnchorStyles.Bottom | AnchorStyles.Right)));
 			button1.Location = new System.Drawing.Point(637, 274);
 			button1.Name = "button1";
@@ -111,9 +121,10 @@ namespace SupportRequest
 			button1.TabIndex = 4;
 			button1.Text = "Submit";
 			button1.UseVisualStyleBackColor = true;
-			button1.Click += new System.EventHandler(delegate { button1.Enabled = false; submit(titlebox.Text, descriptionbox.Text, urgentbox.Checked.ToString()); button1.Enabled = true; });
+			button1.Click += new System.EventHandler(delegate { submit(); });
 
 			// Urgent ticket checkbox
+			urgentbox = new CheckBox();
 			urgentbox.Anchor = ((AnchorStyles)((AnchorStyles.Bottom | AnchorStyles.Left)));
 			urgentbox.AutoSize = true;
 			urgentbox.Location = new System.Drawing.Point(13, 274);
@@ -137,8 +148,7 @@ namespace SupportRequest
 			win.Name = "Win";
 			win.StartPosition = FormStartPosition.CenterScreen;
 			win.Text = "Support Request";
-			win.ResumeLayout(false);
-			win.PerformLayout();
+
 			win.ShowDialog();
 		}
 	}
